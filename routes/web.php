c2dfc89a91c72b4216eaacf08fa21409b8c96bbf
edmundogabrielmartinez <mundogabrielmart@gmail.com>
2025-06-24 18:59:40 -0600

    commit final

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,25 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
-Route::resource('categories', CategoryController::class);
-Route::resource('products', ProductController::class);
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Cambiamos la ruta principal para que redirija a la lista de productos
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('products.index');
 });
+
+// Rutas para el CRUD de Categorías
+Route::resource('categories', CategoryController::class);
+
+// Rutas para el CRUD de Productos
+Route::resource('products', ProductController::class);
